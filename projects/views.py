ab31c7ded96fd055650cbce00bb139d43095e542
Htifano <91378921+Kuretha@users.noreply.github.com>
2024-04-19 05:51:14 +0700

    basic skeleton before migrate

---
+++ projects/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+  return render(request, 'index.html')
+
+def projects(request):
+  projects = [
+    {
+      'id':1,
+      'title':"PROJECT A"
+    },
+    {
+      'id':2,
+      'title':"PROJECT B"
+    }
+  ]
+  # context = {'project1': '1st Project'}
+  context = projects
+  return render(request, 'projects/projects.html', context)
