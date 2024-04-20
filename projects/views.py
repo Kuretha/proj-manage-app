8af527539acfa398e30548b8f8c7c164f35f68aa
Htifano <91378921+Kuretha@users.noreply.github.com>
2024-04-20 20:22:05 +0700

    done creating crud in task and project, user manage wip

--- projects/views.py
+++ projects/views.py
@@ -1,20 +1,80 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Project, Task
+from .forms import TaskForm
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
 
 # Create your views here.
 def home(request):
   return render(request, 'index.html')
 
-def projects(request):
-  projects = [
-    {
-      'id':1,
-      'title':"PROJECT A"
-    },
-    {
-      'id':2,
-      'title':"PROJECT B"
-    }
-  ]
-  # context = {'project1': '1st Project'}
-  context = projects
-  return render(request, 'projects/projects.html', context)
+def projectList(request):
+  projects = Project.objects.all()
+
+  context = {'projects':projects}
+  return render(request, 'projects/projects.html',context)
+
+def projectDetail(request,pk):
+  project = get_object_or_404(Project, id=pk)
+  project_tasks = project.task_set.all()
+  
+  context = {'project':project,'project_tasks':project_tasks}
+  return render(request, 'projects/project-detail.html',context)
+
+def taskList(request):
+  user_tasks =Task.objects.filter(assignee=request.user)
+  tasks = Task.objects.filter(assignee=None)
+
+  context = {'tasks':tasks,'user_tasks':user_tasks}
+  return render(request, 'projects/tasks.html',context)
+
+def taskDetail(request,pk):
+  task = get_object_or_404(Task, id=pk)
+  context = {'task':task}
+  return render(request, 'projects/task-detail.html',context)
+
+def taskCreate(request):
+  form = TaskForm
+  if request.method == "POST":
+      form =TaskForm(request.POST)
+      if form.is_valid():
+          form.save()  
+          return redirect('tasks')
+
+  context = {'form':form}
+  return render(request, 'projects/task-create.html',context)
+
+class ProjectCreateView(CreateView):
+  model = Project
+  fields = ["name","description"]
+  template_name = 'projects/project_create_form.html'
+  success_url = reverse_lazy('projects')
+
+class ProjectUpdateView(UpdateView):
+    model = Project
+    template_name = 'projects/project_update_form.html'
+    fields = ["name","description"]
+    success_url = reverse_lazy('projects')
+
+class TaskUpdateView(UpdateView):
+  model = Task
+  fields = ["title","description","project","assignee","due_date","status"]
+  template_name = 'projects/task_update_form.html'
+  success_url = reverse_lazy('tasks')
+  
+class TaskDeleteView(DeleteView):
+  model = Task
+  template_name = 'projects/task_confirm_delete.html'
+  success_url = reverse_lazy('tasks')
+
+class ProjectDeleteView(DeleteView):
+  model = Project
+  template_name = 'projects/project_confirm_delete.html'
+  success_url = reverse_lazy('projects')
+  
+def joinTask(request,pk):
+  task =Task.objects.get(id=pk)
+  task.assignee=request.user
+  task.save()
+  return redirect('tasks')
